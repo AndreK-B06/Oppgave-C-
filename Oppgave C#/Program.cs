07cb9ba104ca6cd38dd1386a47a69ef01206a352
André <137167063+AndreK-B06@users.noreply.github.com>
2024-05-06 13:01:42 +0200

    Add project files.

---
+++ Oppgave C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Oppgave_C_
+{
+     class Program
+    {
+        static void Main()
+        {
+            // Variablels
+            int userAge = 50;
+            //
+            double userHight = 150.1;
+            //
+            string userName = "Leah";
+            //Caling Variabels
+            Console.WriteLine("Age:" + userAge);
+            Console.WriteLine("Hight:" + userHight);
+            Console.WriteLine("Name:" + userName);
+        }
+    }
+}
